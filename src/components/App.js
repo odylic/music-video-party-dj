5d3c1dda5315685b0c422671979bf6e1bf483d3e
Jimmy <odylic.lin@gmail.com>
2022-01-07 20:30:10 -0800

    first try for the iframe

--- src/components/App.js
+++ src/components/App.js
@@ -2,31 +2,14 @@ import React from "react";
 import "../App.scss";
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  // // React.useEffect(() => {
-  // //   fetch('/api')
-  // //     .then((res) => {
-  // //       console.log(res)
-  // //       return res.json()
-  // //     })
-  // //     .then((data) => setData(data.message));
-  // // }, []);
-
-  React.useEffect(async () => {
-    const response1 = await fetch("/api");
-    const response2 = await response1.json();
-    const data = await response2;
-    console.log(data.message);
-    setData(data.message);
-  }, []);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <p>{!data ? "Loading..." : data}</p>
-        React
-      </header>
+      <iframe
+        src="http://www.cnn.com/embed"
+        allow="fullscreen"
+        height="1000px"
+        width="500px"
+      ></iframe>
     </div>
   );
 }
